98c4780e245cfcaaecb258eed33f2004821fbe21
Alessio Malerba <38223375+N0g4D@users.noreply.github.com>
2023-05-25 18:11:01 +0200

    change of protocol
    Crysm main concept is better be built with ERC721... I guess...

--- contracts/Crysm.sol
+++ contracts/Crysm.sol
@@ -1,122 +1,160 @@
+//work in progress...
+
 // SPDX-License-Identifier: MIT
+/*
++ + + - - - - - - - - - - - - - - - - - - - - - - - - - - - ++ - - - - - - - - - - - - - - - - - - - - - - - - - - + + +
++                                                                                                                      +
++                                                                                                                      +
+.                                                                                                                      .
+.                                                    ####-                                                             .
+.                                        :*##:   .###-...:##                                                           .
+.                                        +#.:# :#+:........+#####+                                                     .
+.                                       .:#..#*=.................:#.                                                   .
+.                                     .#..*:.........:.........=-.-#                                                   .
+.                                      :+#-....:+**+^^+***__=**#:.*#                                                   .
+.                                      .#:.....:#              ^#.#                                                    .
+.                                      #:....-#^                 #                                                     .
+.                                      #-.....#    ++             +   ++                                               .
+.                                      .#.....+   +##+            .* +##+                                              .
+.                                       #.....#    ++  .######**.  #  ++                                               .
+.                                       #*  ^#^       *####--  ##. #                                                   .
+.                                      #:  #.         *####--  #*  #                                                   .
+.                                      #.   # :.       .###--  -# .+                                                   .
+.                                       #.   .#          ###--  # #                                                    .
+.                                        ^##^  #       .###--  =# *                                                    .
+.                                            #**#.     ###--  -*+**#                                                   .
+.                                            #***####**###--  +#****#                                                  .
+.                                           .##*********###--  #**##.                                                  .
+.                                          #************###--  #*****#                                                 .
+.                    ..:..                #*****#*******###--  #**#****###* *###                                       .
+.               .####*^^^^*###.          ##*****#******###--  #***#***#  :# #  :#                                      .
+.              :*             =#.        #******#*******##--  #***#***#  .# #  .#                 .####.               .
+.               ++#   ######.   #-  .*#####*****#*########--  #***#####  .# #  .#   .#######.   #*^   .^#+             .
+.                +#   #::::..#   #.#^^      ^#**#^       ^#- #**#^       .# #  .# +#^       ^# #+        #+            .
+.                .#   #      #   ##    .##.  .##.   .##.   # *#   .##.   .# #  .##.   -+#+.  # #   :######:            .
+.                .#   #     .#   #    #:::#   #.   #:::#   ###   #::::#  .# #  .##  :#####+  #.##     :...             .
+.                .#   #    .#.  :#   #   .#  .#   #    #   ##    #^   #  .# #  .##  .^     .##. *#+:.    .#            .
+.                .#   #####^    ##   #  :#.  .#   #  .#    ##    #   #.  .# #  .##   .=*######..##+###.   .#           .
+.                .#   ..      .###.   ###   .%#.   ###    *#-#    ###     # #  .##*   .      ####   ^     .#           .
+.                 #.   ..:-#####* #.      .*#  ##.      .##. :#._     _### ##.  #**#.     .:##. ##.     -##            .
+.                  ############*   =########    :########-     +#######^ ### ##### ^######++#^   ^#######+             .
+.                    ^*######*^      ^*##*^       ^*##*^         ^*###*^ ^##^ ^###^  ^*####*^      ^*##*^              .
++                                                                                                                      +
++                                                                                                                      +
++ + + - - - - - - - - - - - - - - - - - - - - - - - - - - - ++ - - - - - - - - - - - - - - - - - - - - - - - - - - + + +
+*/
 pragma solidity ^0.8.0;
 
-import "../node_modules/@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
-import "./GameOwner.sol";
-import "../node_modules/@openzeppelin/contracts/security/Pausable.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
+import "../node_modules/@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "../node_modules/@openzeppelin/contracts/utils/Counters.sol";
 
-contract Crysm is ERC1155Supply, GameOwner, Pausable {
+contract PudgyPenguins is ERC721Enumerable, Ownable{
+    using SafeMath for uint256;
+    using Counters for Counters.Counter;
 
-    mapping(uint24 => bool) private isLeadColorTaken;
+    Counters.Counter private _tokenIdTracker;
 
-    uint8[12] public redValues;
-    uint8[12] public greenValues;
-    uint8[12] public blueValues;
+    uint256 public constant MAX_ELEMENTS = 8888;
+    uint256 public constant PRICE = 3 * 10**16;
+    uint256 public constant MAX_BY_MINT = 20;
+    uint256 public constant reveal_timestamp = 1627588800; // Thu Jul 29 2021 20:00:00 GMT+0000
+    address public constant creatorAddress = 0x6F84Fa72Ca4554E0eEFcB9032e5A4F1FB41b726C;
+    address public constant devAddress = 0xcBCc84766F2950CF867f42D766c43fB2D2Ba3256;
+    string public baseTokenURI;
 
-    constructor() ERC1155("TODO-https://sunflower-land.com/play/erc1155/{id}.json") payable {
-        gameRoles[msg.sender] = true;
+    event CreatePenguin(uint256 indexed id);
+    constructor(string memory baseURI) ERC721("PudgyPenguins", "PPG") {
+        setBaseURI(baseURI);
+        pause(true);
     }
 
-    function getLeadRedValue() public view returns (uint8) {
-        
+    modifier saleIsOpen {
+        require(_totalSupply() <= MAX_ELEMENTS, "Sale end");
+        if (_msgSender() != owner()) {
+            require(!paused(), "Pausable: paused");
+        }
+        _;
+    }
+    function _totalSupply() internal view returns (uint) {
+        return _tokenIdTracker.current();
+    }
+    function totalMint() public view returns (uint256) {
+        return _totalSupply();
+    }
+    function mint(address _to, uint256 _count) public payable saleIsOpen {
+        uint256 total = _totalSupply();
+        require(total + _count <= MAX_ELEMENTS, "Max limit");
+        require(total <= MAX_ELEMENTS, "Sale end");
+        require(_count <= MAX_BY_MINT, "Exceeds number");
+        require(msg.value >= price(_count), "Value below price");
+
+        for (uint256 i = 0; i < _count; i++) {
+            _mintAnElement(_to);
+        }
+    }
+    function _mintAnElement(address _to) private {
+        uint id = _totalSupply();
+        _tokenIdTracker.increment();
+        _safeMint(_to, id);
+        emit CreatePenguin(id);
+    }
+    function price(uint256 _count) public pure returns (uint256) {
+        return PRICE.mul(_count);
     }
 
-    function setURI(string memory newuri) public onlyOwner returns (bool) {
-        _setURI(newuri);
-        return true;
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseTokenURI;
     }
 
-    function gameMint(
-        address to,
-        uint256[] memory ids,
-        uint256[] memory amounts,
-        bytes memory data
-    ) public onlyGame returns (bool) {
-        _mintBatch(to, ids, amounts, data);
-        return true;
+    function setBaseURI(string memory baseURI) public onlyOwner {
+        baseTokenURI = baseURI;
     }
 
-    function gameBurn(
-        address to,
-        uint256[] memory ids,
-        uint256[] memory amounts
-    ) public onlyGame returns (bool) {
-        _burnBatch(to, ids, amounts);
-        return true;
-    }
+    function walletOfOwner(address _owner) external view returns (uint256[] memory) {
+        uint256 tokenCount = balanceOf(_owner);
 
-    function gameTransferFrom(
-        address from,
-        address to,
-        uint256[] memory ids,
-        uint256[] memory amounts,
-        bytes memory data
-    ) public onlyGame returns (bool) {
-        _safeBatchTransferFrom(from, to, ids, amounts, data);
-        return true;
-    }
-
-    function gameSetApproval(
-        address owner,
-        address operator,
-        bool approved
-    ) public onlyGame returns (bool) {
-        _setApprovalForAll(owner, operator, approved);
-        return true;
-    }
-
-    /**
-     * Fetch supply for multiple tokens
-     */
-    function totalSupplyBatch(uint256[] memory ids)
-        public
-        view
-        returns (uint256[] memory)
-    {
-        uint256[] memory batchSupply = new uint256[](ids.length);
-
-        for (uint256 i = 0; i < ids.length; ++i) {
-            batchSupply[i] = totalSupply(ids[i]);
+        uint256[] memory tokensId = new uint256[](tokenCount);
+        for (uint256 i = 0; i < tokenCount; i++) {
+            tokensId[i] = tokenOfOwnerByIndex(_owner, i);
         }
 
-        return batchSupply;
+        return tokensId;
     }
 
-    /**
-     * @dev See {ERC1155-_beforeTokenTransfer}.
-     *
-     * Requirements:
-     *
-     * - the contract must not be paused.
-     */
+    function pause(bool val) public onlyOwner {
+        if (val == true) {
+            _pause();
+            return;
+        }
+        _unpause();
+    }
+
+    function withdrawAll() public payable onlyOwner {
+        uint256 balance = address(this).balance;
+        require(balance > 0);
+        _widthdraw(devAddress, balance.mul(35).div(100));
+        _widthdraw(creatorAddress, address(this).balance);
+    }
+
+    function _widthdraw(address _address, uint256 _amount) private {
+        (bool success, ) = _address.call{value: _amount}("");
+        require(success, "Transfer failed.");
+    }
+
     function _beforeTokenTransfer(
-        address operator,
         address from,
         address to,
-        uint256[] memory ids,
-        uint256[] memory amounts,
-        bytes memory data
-    ) internal virtual override {
-        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
-
-        require(!paused(), "ERC1155Pausable: token transfer while paused");
+        uint256 tokenId
+    ) internal virtual override(ERC721, ERC721Enumerable, ERC721Pausable) {
+        super._beforeTokenTransfer(from, to, tokenId);
     }
-}
 
-/*
-contract Crysm is ERC1155 {
-    uint256 public constant GOLD = 0;
-    uint256 public constant SILVER = 1;
-    uint256 public constant THORS_HAMMER = 2;
-    uint256 public constant SWORD = 3;
-    uint256 public constant SHIELD = 4;
-
-    constructor() ERC1155("https://game.example/api/item/{id}.json") {
-        _mint(msg.sender, GOLD, 10**18, "");
-        _mint(msg.sender, SILVER, 10**27, "");
-        _mint(msg.sender, THORS_HAMMER, 1, "");
-        _mint(msg.sender, SWORD, 10**9, "");
-        _mint(msg.sender, SHIELD, 10**9, "");
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, ERC721Enumerable) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
+    
 }
-*/
